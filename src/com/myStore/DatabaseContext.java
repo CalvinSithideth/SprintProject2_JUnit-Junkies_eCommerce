7225d5c873d3a57b8c23f86b9979a4fea44c18d5
148646 <148646@G917LLAX.ustr.com>
2020-03-05 13:50:04 -0800

    Refactoring for database context

---
+++ src/com/myStore/DatabaseContext.java
@@ -0,0 +1,52 @@
+package com.myStore;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class DatabaseContext {
+	private String url = "jdbc:mysql://localhost:3306/eCommerce_JUnitJunkies";
+	private static final String USERNAME = "default";
+	private static final String PASSWORD = "COVA";
+	private Connection con;
+
+	public DatabaseContext() throws SQLException {
+		con = DriverManager.getConnection(url, USERNAME, PASSWORD);
+	}
+
+	public void runStatement() {
+		// set up connections and statements
+		Scanner sc = new Scanner(System.in);
+		try {
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(getUserInput(sc));
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int columns = rsmd.getColumnCount();
+
+			// iterate over resultset
+			// Need to update to fit sql column standards
+			for (int i = 1; i <= columns; i++) {
+				String name = rsmd.getColumnName(i);
+				String value = rs.getString(i);
+				System.out.println(name + ": " + value);
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} catch (Exception e) {
+			System.out.println("You done goofed");
+		}
+	}
+
+	private static String getUserInput(Scanner sc) {
+		// prompts user for SMART SQL statement
+		System.out.println("\nWelcome to the ECommerce JUnit Junkies Application!"
+				+ "\n-------------------------------------------------"
+				+ "\nPlease write a valid SQL statement to find your info");
+		return sc.nextLine();
+	}
+
+}
