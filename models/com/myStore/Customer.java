f204f9b1e474c899c28412f31354770bab1dc81b
Calvin Sithideth <47773333+CalvinSithideth@users.noreply.github.com>
2020-03-09 15:02:38 -0700

    added toString, changed zip and phone to strings

--- models/com/myStore/Customer.java
+++ models/com/myStore/Customer.java
@@ -7,12 +7,13 @@ public class Customer {
 	private String addressLine2;
 	private String city;
 	private String state;
-	private int zipCode;
+	private String zipCode;
 	private String Email;
-	private int phoneNumber;
-	private int billingInfo;
+	private String phoneNumber;
+	private String billingInfo;
 	public Customer(int customerID, String name, String addressLine1, String addressLine2, String city, String state,
-			int zipCode, String email, int phoneNumber, int billingInfo) {
+			String zipCode, String email, String phoneNumber, String billingInfo)
+	{
 		this.customerID = customerID;
 		this.name = name;
 		this.addressLine1 = addressLine1;
@@ -24,64 +25,112 @@ public class Customer {
 		this.phoneNumber = phoneNumber;
 		this.billingInfo = billingInfo;
 	}
-	public int getCustomerID() {
+
+	public int getCustomerID()
+	{
 		return customerID;
 	}
-	public void setCustomerID(int customerID) {
+
+	public void setCustomerID(int customerID)
+	{
 		this.customerID = customerID;
 	}
-	public String getName() {
+
+	public String getName()
+	{
 		return name;
 	}
-	public void setName(String name) {
+
+	public void setName(String name)
+	{
 		this.name = name;
 	}
-	public String getAddressLine1() {
+
+	public String getAddressLine1()
+	{
 		return addressLine1;
 	}
-	public void setAddressLine1(String addressLine1) {
+
+	public void setAddressLine1(String addressLine1)
+	{
 		this.addressLine1 = addressLine1;
 	}
-	public String getAddressLine2() {
+
+	public String getAddressLine2()
+	{
 		return addressLine2;
 	}
-	public void setAddressLine2(String addressLine2) {
+
+	public void setAddressLine2(String addressLine2)
+	{
 		this.addressLine2 = addressLine2;
 	}
-	public String getCity() {
+
+	public String getCity()
+	{
 		return city;
 	}
-	public void setCity(String city) {
+
+	public void setCity(String city)
+	{
 		this.city = city;
 	}
-	public String getState() {
+
+	public String getState()
+	{
 		return state;
 	}
-	public void setState(String state) {
+
+	public void setState(String state)
+	{
 		this.state = state;
 	}
-	public int getZipCode() {
+
+	public String getZipCode()
+	{
 		return zipCode;
 	}
-	public void setZipCode(int zipCode) {
+
+	public void setZipCode(String zipCode)
+	{
 		this.zipCode = zipCode;
 	}
-	public String getEmail() {
+
+	public String getEmail()
+	{
 		return Email;
 	}
-	public void setEmail(String email) {
+
+	public void setEmail(String email)
+	{
 		Email = email;
 	}
-	public int getPhoneNumber() {
+
+	public String getPhoneNumber()
+	{
 		return phoneNumber;
 	}
-	public void setPhoneNumber(int phoneNumber) {
+
+	public void setPhoneNumber(String phoneNumber)
+	{
 		this.phoneNumber = phoneNumber;
 	}
-	public int getBillingInfo() {
+
+	public String getBillingInfo()
+	{
 		return billingInfo;
 	}
-	public void setBillingInfo(int billingInfo) {
+
+	public void setBillingInfo(String billingInfo)
+	{
 		this.billingInfo = billingInfo;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Customer [customerID=" + customerID + ", name=" + name + ", addressLine1=" + addressLine1
+				+ ", addressLine2=" + addressLine2 + ", city=" + city + ", state=" + state + ", zipCode=" + zipCode
+				+ ", Email=" + Email + ", phoneNumber=" + phoneNumber + ", billingInfo=" + billingInfo + "]";
 	}
 }
